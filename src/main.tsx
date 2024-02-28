de79b8ac6bbce380a363196c1a79321f6657fea6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-02-28 16:41:30 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app.tsx";
+import { App } from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
